a371494f9df6366bb4e9302b8b3c968735faab6a
Giovanni Venturi <buildyourapp@gmail.com>
2023-05-24 12:04:42 +0200

    first tool for bash: generate random MAC address splitted in more files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tools
+
+In this repository, you can find some bash and python tools for your Linux system.
---
+++ bash/generate_different_mac_address.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+hexchars="0123456789ABCDEF"
+MACSFILE_PREFIX="$HOME/macs0"
+if [ -z "$MACS_PER_FILE" ];
+then
+  MACS_PER_FILE=200
+fi
+
+if [ -z "$NUMBER_OF_FILES" ];
+then
+  NUMBER_OF_FILES=5
+fi
+rm -f $MACSFILE_PREFIX?.txt
+
+function isInFile
+{
+  if [ -z "$1" ];
+  then
+    echo "*** ERROR: no input MAC address"
+    exit 1
+  fi
+
+  II=1
+  while [ $II -le $NUMBER_OF_FILES ];
+  do
+    FILENAME="$MACSFILE_PREFIX$II.txt"
+    if [ -f $FILENAME ];
+    then
+      while read line
+      do
+        if [ "z$1" == "z$lilne" ];
+        then
+          echo "*** FOUND an existing MAC address"
+          return 1
+        fi
+      done < $FILENAME
+    else
+      return 0
+    fi
+    II=$((II+1))
+  done
+  return 0
+}
+export -f isInFile
+
+x=0
+JJ=1
+MAC_FILENAME="$MACSFILE_PREFIX$JJ.txt"
+while [ $x -lt $MACS_PER_FILE ] && [ $JJ -le $NUMBER_OF_FILES ];
+do
+  mac="00:0C:29$(for i in {1..6}; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g')"
+  isInFile $mac
+  if [ $? -eq 0 ];
+  then
+    echo "$mac" >> $MAC_FILENAME
+    x=$((x+1))
+    echo "$x: $mac"
+    if [ $x -eq $MACS_PER_FILE ];
+    then
+      JJ=$((JJ+1))
+      x=0
+      MAC_FILENAME="$MACSFILE_PREFIX$JJ.txt"
+    fi
+  fi
+done
