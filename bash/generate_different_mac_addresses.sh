a6d5670d7618aa98a232f1e411c6cf84bd381406
Giovanni Venturi <buildyourapp@gmail.com>
2023-06-01 11:51:39 +0200

    added a configuration environment for generate_different_mac_addresses.sh

--- bash/generate_different_mac_addresses.sh
+++ bash/generate_different_mac_addresses.sh
@@ -1,30 +1,85 @@
 #!/bin/bash
 
+if [ -z "$DEBUG" ];
+then
+  DEBUG=0
+else
+  case $DEBUG in
+    "0"|"1")
+      ;;
+    "y"|"yes"|"true")
+      DEBUG=1
+      ;;
+    "n"|"no"|"false")
+      DEBUG=0
+      ;;
+    *)
+      DEBUG=0
+      echo "DEBUG is NOT assigned correctly, used 'false' value in place of the wrong value"
+  esac
+fi
+
+if [ -z "$DESTDIR" ];
+then
+  DESTDIR="$HOME"
+fi
+
+if [ -d "$DESTDIR" ];
+then
+  DESTDIR="$DESTDIR/mac_addresses"
+  mkdir -p $DESTDIR
+  echo "MAC list files is going to be written to $DESTDIR"
+else
+  echo "$DESTDIR does NOT exist"
+  exit 1
+fi
+
+if [ -z "$MAC_PREFIX" ];
+then
+  MAC_PREFIX="00:0C:29"
+  echo "I assigned $MAC_PREFIX as a MAC prefix"
+else
+  size=${#MAC_PREFIX}
+  case $size in
+    "3"|"5"|"7"|"9")
+      echo "I assigned $MAC_PREFIX as a MAC prefix"
+      ;;
+    *)
+      echo "$MAC_PREFIX is NOT correct as a MAC prefix, replacing it with 00:0C:29"
+      MAC_PREFIX="00:0C:29"
+      ;;
+  esac
+fi
+
 hexchars="0123456789ABCDEF"
-MACSFILE_PREFIX="$PWD/macs0"
+MACSFILE_PREFIX="$DESTDIR/macs0"
 if [ -z "$MACS_PER_FILE" ];
 then
   MACS_PER_FILE=200
 fi
+
+case $MACS_PER_FILE in
+  "1")
+    echo "I'm going to generate $MACS_PER_FILE different MAC address per file"
+    ;;
+  *)
+    echo "I'm going to generate $MACS_PER_FILE different MAC addresses per file"
+    ;;
+esac
 
 if [ -z "$NUMBER_OF_FILES" ];
 then
   NUMBER_OF_FILES=5
 fi
 rm -f $MACSFILE_PREFIX?.txt
-
-if [ -z "$DEBUG" ];
-then
-  DEBUG=0
-else
-  case "$DEBUG" in
-    "0"|"1")
-      ;;
-    *)
-      DEBUG=0
-      ;;
-  esac
-fi
+case $NUMBER_OF_FILES in
+  "1")
+    echo "I'm going to generate $NUMBER_OF_FILES file"
+    ;;
+  *)
+    echo "I'm going to generate $NUMBER_OF_FILES files"
+    ;;
+esac
 
 function isInFile
 {
@@ -44,10 +99,7 @@ function isInFile
       do
         if [ "z$1" == "z$lilne" ];
         then
-          if [ $DEBUG -eq 1 ];
-          then
-            echo "*** FOUND an existing MAC address: $1"
-          fi
+          echo "*** FOUND an existing MAC address"
           return 1
         fi
       done < $FILENAME
@@ -65,7 +117,7 @@ JJ=1
 MAC_FILENAME="$MACSFILE_PREFIX$JJ.txt"
 while [ $x -lt $MACS_PER_FILE ] && [ $JJ -le $NUMBER_OF_FILES ];
 do
-  mac=$(printf "00:0C:29:%02x:%02x:%02x\n" $(( $RANDOM % 256 )) $(( $RANDOM % 256 )) $(( $RANDOM % 256)); )
+  mac="$MAC_PREFIX$(for i in {1..6}; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g')"
   isInFile $mac
   if [ $? -eq 0 ];
   then
@@ -77,7 +129,6 @@ do
     fi
     if [ $x -eq $MACS_PER_FILE ];
     then
-      echo "... wrote $MAC_FILENAME file"
       JJ=$((JJ+1))
       x=0
       MAC_FILENAME="$MACSFILE_PREFIX$JJ.txt"
