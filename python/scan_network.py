20f20e4778770ca75596c25a4f7efee295d7c580
Giovanni Venturi <buildyourapp@gmail.com>
2023-06-06 17:40:44 +0200

    add a function to scan a subnet

--- python/scan_network.py
+++ python/scan_network.py
@@ -3,18 +3,33 @@
 import platform    # let you get the operating system name
 import subprocess  # let you execute a shell command
 
-if platform.system().lower() == 'windows':
-    packets_opt = '-n'
-else:
-    packets_opt = '-c'
 
-base_ip = '192.168.0'
-i = 1
-while i < 255:
-    command = ['ping', packets_opt, '1', '-W1', f'{base_ip}.{i}']
-    try:
-        if subprocess.run(args=command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0:
-            print(f'{base_ip}.{i} reachable')
-    except KeyboardInterrupt:
-        break
-    i += 1
+def scanning_network(base_ip = '192.168.0', show_reachable_ips=False):
+    ips = []
+    if platform.system().lower() == 'windows':
+        packets_opt = '-n'
+    else:
+        packets_opt = '-c'
+
+    i = 1
+    while i < 255:
+        new_ip = f'{base_ip}.{i}'
+        command = ['ping', packets_opt, '1', '-W1', new_ip ]
+        try:
+            if subprocess.run(args=command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0:
+                if show_ips:
+                    print(f'{base_ip}.{i} reachable')
+                ips.append(new_ip)
+        except KeyboardInterrupt:
+            break
+        if not show_ips:
+            print(end='.', flush=True)
+        i += 1
+    print()
+    return ips
+
+
+ip_hosts = scanning_network(show_reachable_ips=True)
+print('reachable IPs:')
+for ip in ip_hosts:
+    print(ip)
