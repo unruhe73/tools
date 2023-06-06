e4bb7a83eeee3efd9bbd916b2ab838f279b4e04f
Giovanni Venturi <buildyourapp@gmail.com>
2023-06-06 16:01:56 +0200

    adding scanning network bash script: checking if an host is reachable or not by using ping

---
+++ bash/scan_network.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BASE_IP="192.168.0"
+i=1
+while [ $i -lt 255 ];
+do
+  ping -c 1 $BASE_IP.$i 1>/dev/null 2>/dev/null
+  if [ $? -eq 0 ];
+  then
+    echo "$BASE_IP.$i reachable"
+  fi
+  i=$((i+1))
+done
