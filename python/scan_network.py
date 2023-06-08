93ba8fb4b60e4719efcd254137ec7a50cf58382f
Giovanni Venturi <buildyourapp@gmail.com>
2023-06-08 13:14:51 +0200

    moved to a real command line adding argparse

--- python/scan_network.py
+++ python/scan_network.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
-import ipaddress
-import platform    # let you get the operating system name
-import subprocess  # let you execute a shell command
-from progress.bar import Bar
-from time import sleep
+import argparse               # let you get parameters from the command line
+import sys                    # let you access to system stuff as argv command line parameters
+import ipaddress              # let you use IP addresses for hosts/networks
+import platform               # let you get the operating system name
+import subprocess             # let you execute a shell command
+from progress.bar import Bar  # let you use a progress bar
+from time import sleep        # let you use sleep to delay
 
 
-def scanning_network(base_ip='192.168.0.0/24', show_reachable_ips=False):
+def scanning_network(network_ip='192.168.0.0/24', show_reachable_ips=False):
     ips = []
     try:
-        network_ipv4 = ipaddress.IPv4Network(base_ip)
+        network_ipv4 = ipaddress.IPv4Network(network_ip)
     except ValueError:
         print('address/netmask is invalid for IPv4:', base_ip)
         return ips
 
-    print(f"I'm going to scan {base_ip} network")
+    print(f"I'm going to scan {network_ip} network")
     if not show_reachable_ips:
         bar = Bar('Processing', max=len(list(network_ipv4.hosts())))
 
@@ -35,18 +37,19 @@ def scanning_network(base_ip='192.168.0.0/24', show_reachable_ips=False):
             break
         if not show_reachable_ips:
             bar.next()
-    bar.finish()
+    if not show_reachable_ips:
+        bar.finish()
     return ips
 
 
-def scanning_network_write_to_file(base_ip='192.168.0.0/24', show_reachable_ips=False, write_to_file='reachable_ips.txt'):
-    ip_hosts = scanning_network(base_ip=base_ip, show_reachable_ips=False)
+def scanning_network_write_to_file(network_ip='192.168.0.0/24', show_reachable_ips=False, write_to_file='reachable_ips.txt'):
+    ip_hosts = scanning_network(network_ip=network_ip, show_reachable_ips=show_reachable_ips)
     if len(ip_hosts) > 0:
         if write_to_file:
             with open(write_to_file, 'w+') as f:
                 f.write('\n'.join(ip_hosts))
                 f.write('\n')
-            print(f'See file {write_to_file} for output')
+            print(f'\nI saved on-line hosts on the text file {write_to_file}')
         else:
             sleep(2)
             print('reachable IPs:')
@@ -56,4 +59,30 @@ def scanning_network_write_to_file(base_ip='192.168.0.0/24', show_reachable_ips=
         print('No reachable IPs')
 
 
-scanning_network_write_to_file()
+def get_parameters():
+    network_ip = '192.168.0.0/24'
+    show_reachable_ips = False
+    write_to_file = 'reachable_ips.txt'
+
+    parser = argparse.ArgumentParser(description='A python script to scan on-line hosts into a LAN')
+    pyname = sys.argv[0]
+    parser.add_argument('-n', '--network', help='assign the network to scan, example: 192.168.0.0/24', type=str)
+    parser.add_argument('-o', '--output-file', help='write on-line hosts to a file', type=str)
+    parser.add_argument('-s', '--show-reachable-ips', help='you want to get the on-line hosts on the terminal', action="store_true")
+    args = parser.parse_args()
+
+    if args.network:
+        network_ip = args.network
+
+    if args.output_file:
+        write_to_file = args.output_file
+
+    if args.show_reachable_ips:
+        show_reachable_ips = True
+
+    return network_ip, show_reachable_ips, write_to_file
+
+
+if __name__ == '__main__':
+    network_ip, show_reachable_ips, filename = get_parameters()
+    scanning_network_write_to_file(network_ip=network_ip, show_reachable_ips=show_reachable_ips, write_to_file=filename)
