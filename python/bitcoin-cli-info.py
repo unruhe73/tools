490fad81fd1c1a9d8632cfa3b14f910ec8f6d600
Giovanni Venturi <buildyourapp@gmail.com>
2023-06-28 16:21:04 +0200

    if bitcon-cli doesn't exist...

--- python/bitcoin-cli-info.py
+++ python/bitcoin-cli-info.py
@@ -3,34 +3,42 @@
 import subprocess
 import json
 
-command = ['/usr/local/bin/bitcoin-cli', '-conf=/etc/bitcoin.conf', '-getinfo']
-res = subprocess.run(args=command, capture_output=True)
-output = res.stdout.decode('UTF-8')
-ret_value = res.returncode
+bitcoin_cli = '/usr/local/bin/bitcoin-cli'
 
-getinfo = {}
-if ret_value == 0:
-    lines = output.split('\n')
-    for line in lines:
-        if line:
-            item = line.split(':')
-            getinfo[item[0].strip()] = item[1].strip()
-    print(5*'*' + ' getinfo:')
-    for i in getinfo.keys():
-        print(i + ': ' + str(getinfo[i]))
-else:
-    print('Error!')
+command = [bitcoin_cli, '-conf=/etc/bitcoin.conf', '-getinfo']
+try:
+    res = subprocess.run(args=command, capture_output=True)
+    output = res.stdout.decode('UTF-8')
+    ret_value = res.returncode
+
+    getinfo = {}
+    if ret_value == 0:
+        lines = output.split('\n')
+        for line in lines:
+            if line:
+                item = line.split(':')
+                getinfo[item[0].strip()] = item[1].strip()
+        print(5*'*' + ' getinfo:')
+        for i in getinfo.keys():
+            print(i + ': ' + str(getinfo[i]))
+    else:
+        print('Error!')
+except FileNotFoundError:
+    print(bitcoin_cli + ': command not found!')
 
 print()
-command = ['bitcoin-cli', '-conf=/etc/bitcoin.conf', 'getblockchaininfo']
-res = subprocess.run(args=command, capture_output=True)
-output = res.stdout.decode('UTF-8')
-ret_value = res.returncode
+command = [bitcoin_cli, '-conf=/etc/bitcoin.conf', 'getblockchaininfo']
+try:
+    res = subprocess.run(args=command, capture_output=True)
+    output = res.stdout.decode('UTF-8')
+    ret_value = res.returncode
 
-if ret_value == 0:
-    getblockchaininfo = json.loads(output)
-    print(5*'*' + ' getblockchaininfo:')
-    for i in getblockchaininfo.keys():
-        print(i + ': ' + str(getblockchaininfo[i]))
-else:
-    print('Error!')
+    if ret_value == 0:
+        getblockchaininfo = json.loads(output)
+        print(5*'*' + ' getblockchaininfo:')
+        for i in getblockchaininfo.keys():
+            print(i + ': ' + str(getblockchaininfo[i]))
+    else:
+        print('Error!')
+except FileNotFoundError:
+    print(bitcoin_cli + ': command not found!')
