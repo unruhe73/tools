af74ab24189ccd5d2217271a4e2e7580929f5313
Giovanni Venturi <buildyourapp@gmail.com>
2023-08-04 12:07:45 +0200

    manage the exception if the service is not available

--- python/get_public_ip.py
+++ python/get_public_ip.py
@@ -21,12 +21,16 @@ while (get_choice):
         print("\nBye!")
         sys.exit(1)
 
-if service == 1:
-    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-elif service == 2:
-    external_ip = urllib.request.urlopen('https://ifconfig.me').read().decode('utf8')
-elif service == 3:
-    data = urllib.request.urlopen('http://checkip.dyndns.com/').read().decode('utf8')
-    external_ip = re.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').search(data).group(1)
+try:
+    if service == 1:
+        external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    elif service == 2:
+        external_ip = urllib.request.urlopen('https://ifconfg.me').read().decode('utf8')
+    elif service == 3:
+        data = urllib.request.urlopen('http://checkip.dyndns.com/').read().decode('utf8')
+        external_ip = re.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').search(data).group(1)
 
-print(f"Your public IP is {external_ip}")
+    print(f"Your public IP is {external_ip}")
+
+except urllib.error.URLError:
+    print("The service you are requesting seems to be off-line. Are you sure you're connected to the Internet?")
